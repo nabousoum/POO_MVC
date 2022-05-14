99a52f191d95e0d3e6d2cba49d96c7419310d7e1
nabousoum <seynabousoumare98@outlook.fr>
2022-05-14 17:36:26 +0000

    test gitignore

---
+++ core/Session.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Core;
+
+use App\Model\User;
+
+class Session{
+
+    private User $user;
+    public function __construct()
+    {
+        if(session_status()==PHP_SESSION_NONE){
+            session_start();
+        }
+        
+    }
+
+    public function set(string $key, $data){
+        $_SESSION[$key] = $data;
+    }
+
+    public function get(string $key){
+        return $_SESSION[$key];
+    }
+
+    /**
+     * Get the value of user
+     */ 
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set the value of user
+     *
+     * @return  self
+     */ 
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
